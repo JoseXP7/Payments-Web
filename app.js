1f6dc0c9bceaa2ea2512b14c56d4bf48b2570bde
Jose Manuel Graterol Rodriguez <josemanuelgraterolrodriguez@gmail.com>
2023-02-18 15:30:59 -0400

    Add Permitir solo numeros en V E

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
+function onlyNumbers(evt) {
+	if(window.event){
+	keynum = evt.keyCode;
+	}
+	else{
+	keynum = evt.which;
+	}
+	
+	if((keynum > 47 && keynum < 58) || keynum == 8 || keynum== 13)
+	{
+	return true;
+	}
+	else
+	{
+	alert("Ingresar solo numeros");
+	return false;
+	}
+}
 
-
-
-
+// a saber como funciona lo de arriba, solo se que funciona
