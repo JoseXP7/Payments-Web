9a87257a36dc8efe745c1d17248fcfc9d5817ad4
Jose Manuel Graterol Rodriguez <josemanuelgraterolrodriguez@gmail.com>
2023-02-18 19:06:01 -0400

    Validacion de correo y cedula

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+let errorMsg = document.getElementById("error-email")
+let identity = document.getElementById("identification")
+
+let inputEmail = document.getElementById("email")
+inputEmail.addEventListener("input", ()=> {
+	validateEmail(inputEmail.value)
+})
+
+function validateEmail(email) {
+	let expReg = /^[0-9-.]+[0-9-.]+[ucla]+@[gmail]+\.[com]{1,3}$/
+
+	if (expReg.test(email) == true) {
+		inputEmail.style.border = '2px solid green'
+		errorMsg.style.display = 'none'
+		
+	} else {
+		inputEmail.style.border = '2px solid red'
+		errorMsg.style.display = 'block'
+	}
+}
+
 function onlyNumbers(evt) {
 	if(window.event){
 	keynum = evt.keyCode;
@@ -9,6 +30,7 @@ function onlyNumbers(evt) {
 	if((keynum > 47 && keynum < 58) || keynum == 8 || keynum== 13)
 	{
 	return true;
+	identity.style.border = '2px solid green'
 	}
 	else
 	{
@@ -18,3 +40,8 @@ function onlyNumbers(evt) {
 }
 
 // a saber como funciona lo de arriba, solo se que funciona
+
+//formula /^[0-9-.]+[0-9-.]+[ucla]+@[gmail]+\.[com]{1,3}$/ maximo 28 caracteres
+
+
+
