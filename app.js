3f50e90034ff9cd9e10a8bc4ea1e6879caf2d295
Jose Manuel Graterol Rodriguez <josemanuelgraterolrodriguez@gmail.com>
2023-02-18 20:46:10 -0400

    FormSubmit Added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let errorMsg = document.getElementById("error-email")
-let identity = document.getElementById("identification")
+let identidad = document.getElementById("identification")
 
 let inputEmail = document.getElementById("email")
 inputEmail.addEventListener("input", ()=> {
@@ -11,11 +11,29 @@ function validateEmail(email) {
 
 	if (expReg.test(email) == true) {
 		inputEmail.style.border = '2px solid green'
-		errorMsg.style.display = 'none'
+		errorMsg.style.visibility = 'hidden'
 		
 	} else {
 		inputEmail.style.border = '2px solid red'
-		errorMsg.style.display = 'block'
+		errorMsg.style.visibility = 'visible'
+	}
+}
+
+
+let inputFile = document.getElementById("recibo")
+
+inputFile.addEventListener("change", validarInput)
+
+function validarInput() {
+	let ruta = inputFile.value
+	let extPermitidas = /(.jpg|.png|.jpeg)$/i
+
+	if (!extPermitidas.exec(ruta)) {
+		alert("Asegurate de haber seleccionado un archivo .png/.jpg")
+		inputFile.value = ""
+		return false
+	} else {
+		alert("archivo subido")
 	}
 }
 
@@ -30,7 +48,6 @@ function onlyNumbers(evt) {
 	if((keynum > 47 && keynum < 58) || keynum == 8 || keynum== 13)
 	{
 	return true;
-	identity.style.border = '2px solid green'
 	}
 	else
 	{
